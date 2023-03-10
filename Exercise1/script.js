423f8118d7e2b4b4183b7e3661b3e71e2d8cd169
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-03-10 12:01:39 +0700

    Comment code

--- Exercise1/script.js
+++ Exercise1/script.js
@@ -1,12 +1,18 @@
 // Call function
 function ChangeName(){
-    // Variable name
+    // Variable name for value from Prompt
     let name = prompt('What your name?');
+    // Variable myName for element id yourName
     let myName = document.getElementById('yourName');
+    // Change html id myName to name
     myName.innerHTML = name;
     // Talent
+    // Variable el for create element div
     const el = document.createElement('div');
+    // in el element div add text Hello + name from prompt
     el.textContent = 'Hello ' + name;
+    // Variable box for element id box
     const box = document.getElementById('box');
+    // Create el div in element id box
     box.appendChild(el);
 }
